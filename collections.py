7c67b885625b55801b817ae45750857ee239ddcc
CEEXAGON <acejayodom@gmail.com>
2022-05-23 22:14:13 +0700

    f

--- collections.py
+++ collections.py
@@ -1,9 +1,6 @@
-#5 types of collections-container of values:Counter,Namedtuple,orderdirect,defaultdict,deque
 from collections import Counter
+#5 types of collections-container of values:Counter,Namedtuple,orderdirect,defaultdict,deque
 #01--counter
 eyyo = "sdfghjkjhgertyu"
 my_counter = Counter(eyyo)
-print(my_counter.items())
-print(my_counter.keys())
-print(my_counter.values())
-print(my_counter.most_common(1))
+print(my_counter.items)
