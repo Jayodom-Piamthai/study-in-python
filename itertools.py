4ddd0873c64d783ca92f73e91d5ee825e62ae6ea
CEEXAGON <acejayodom@gmail.com>
2022-05-23 22:51:15 +0700

    combii

--- itertools.py
+++ itertools.py
@@ -1,6 +1,6 @@
 #Itertools-collection of tools for handling iterators
 #product,permutations,combunations,accumulate,groupby,infinite itteration
-from itertools import permutations, product,combinations,accumulate,groupby
+from itertools import combinations_with_replacement, permutations, product,combinations,accumulate,groupby
 #01-product-all possible powerset combinations
 a =[1,2]
 b=[5,8]
@@ -13,12 +13,12 @@ print(list(prodRep))
 #02-permutations-all possible arrangement combinations
 set = [1,3,5,8]
 perm = permutations(set)
-permof2 = permutations(set,2)#combinations from lenght of two digits,or specific paramiter
 print(list(perm))
-print(list(permof2))
  
 
-#03 combinations-combinations of arrangements(?????)
-congii = [3,5,9,6]
+#03 combinations-combinations of arrangements(permutations,but better)
+congii = ["a","c","e","r"]
 comb = combinations(congii,2)
-
+comb_wr = combinations_with_replacement(congii,2)#can use same shits
+print(list(comb))
+print(list(comb_wr))
