f0c34fdd0afe7faef3e386183f32a44a9f8d8d46
ACE! <88769732+Jayodom-Piamthai@users.noreply.github.com>
2022-05-25 22:31:01 -0700

    Update Lambda.py

--- Lambda.py
+++ Lambda.py
@@ -1,1 +1,11 @@
 #Lambda expression:ultra complexing,but shorten the lines to a simple 1-3 lines of code
+#structure:
+#Lambda arguments:expression
+
+#Example
+add10  = lambda x: x + 10 
+print(add10(5))
+
+def add10Func(x):
+    return x+10
+print(add10Func(5))
