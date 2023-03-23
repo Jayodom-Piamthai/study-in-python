b0e8fdf5d03874abe285b6f410e5cd74133e31d4
AceJayPiamthai <acejayodom@gmail.com>
2023-03-24 00:41:48 +0700

    yea

--- OOPS.py
+++ OOPS.py
@@ -5,17 +5,22 @@ class Ace:
     def __init__(self,mood,activity): #this is "instance variable",with self as a pointer to the object
         self.mood = mood #making a new "object",this one named
         if activity in ["be productive"]:
-            raise ValueError("Unacceptable bullshitery")
+            raise ValueError("Unacceptable bullshitery") #error exception of your own choosing
         self.activity = activity
+    
+    #string function
+    def __str__(self): #return string
+        return (f"apparently,because ace is {self.mood},he decided to {self.activity} today")
 
 def main():
     ace = today()
-    print(f"today ace is {ace.mood},and he's gonna {ace.activity}")
+    print(ace)
+    
     
 def today():
     mood = input("whats your mood today?:")
     activity = input("what'cha gon do today?:")
     return Ace(mood,activity)
 
-
-main()
+if __name__ == "__main__":
+    main()
